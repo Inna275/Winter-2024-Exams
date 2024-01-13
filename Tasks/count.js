b63377e00320ebe135ccacfe2d604fdf3087caf5
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-13 20:10:17 +0200

    Added brackets for if-statement

--- Tasks/count.js
+++ Tasks/count.js
@@ -6,7 +6,9 @@ const sumNumericValues = (obj) => {
   let sum = 0;
   const values = Object.values(obj);
   for (const value of values) {
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number') {
+      sum += value;
+    }
   }
   return sum;
 };
