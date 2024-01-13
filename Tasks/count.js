041c035d782f4341d4bd5c1013f052801d15b239
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-13 20:33:20 +0200

    Added empty lines for visual separation

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,11 +5,13 @@
 const sumNumericValues = (obj) => {
   let sum = 0;
   const values = Object.values(obj);
+
   for (const value of values) {
     if (typeof value === 'number') {
       sum += value;
     }
   }
+  
   return sum;
 };
 
