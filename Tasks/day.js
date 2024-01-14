40ed6dea636d950065f334c79dfcd2a6742deaee
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 12:10:09 +0200

    Changed let D to const DAYS

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,12 +2,12 @@
 
 'use strict';
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 _parse_day_ = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < DAYS.length; i++) {
+    if (s.startsWith(DAYS[i].toLowerCase())) {
       return i + 1;
     }
   }
