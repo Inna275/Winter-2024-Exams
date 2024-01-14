b5209ea49e6783024635ea124139d83b5d2c264b
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 23:22:54 +0200

    Added spaces for improved readability

--- Tasks/range.js
+++ Tasks/range.js
@@ -6,14 +6,18 @@ const INDEX_OFFSET = 1;
 
 const generateIntArray = (...range) => {
   const [from, to] = range;
+
   if (to < from) {
     return [];
   }
+
   const arrayLength = to - from + INDEX_OFFSET;
   const resultArray = new Array(arrayLength);
+
   for (let i = from; i <= to; i++) {
     resultArray[i - from] = i;
   }
+
   return resultArray;
 };
 
