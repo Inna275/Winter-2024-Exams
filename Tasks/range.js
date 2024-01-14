54f9291ce649a426c46ceb22f487a3c750d39c5c
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 22:13:32 +0200

    Renamed function and added const to it

--- Tasks/range.js
+++ Tasks/range.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-_range = (...Range) => {
+const generateIntArray = (...Range) => {
   let [from, to] = Range;
   if (to >= from) {
     Range = new Array(to - from + 1);
@@ -15,4 +15,4 @@ _range = (...Range) => {
   return Range;
 };
 
-module.exports = _range;
+module.exports = generateIntArray;
