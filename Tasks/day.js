8763e1832a591454749a3cd563e0ad6e6f79b9e7
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 13:26:04 +0200

    Removed magic numbers, added visual separation lines

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,12 +5,16 @@
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (day) => {
+  const OFFSET = 1;
+  const NOT_FOUND = -1;
+
   for (let i = 0; i < DAYS.length; i++) {
     if (day.startsWith(DAYS[i].toLowerCase())) {
-      return i + 1;
+      return i + OFFSET;
     }
   }
-  return -1;
+
+  return NOT_FOUND;
 };
 
 module.exports = getDayNumber;
