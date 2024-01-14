cc0b8cad2956ecf33823b6c4b4ed0c32fc3bef3d
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 15:29:26 +0200

    Changed intersection to arrow syntax, added const

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-intersection = function intersection(object_1, object_2) {
+const intersection = (object_1, object_2) => {
   first_keys = Object.keys(object_1);
   for (attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
