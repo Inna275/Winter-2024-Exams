4e3d2d00500f0003d32cde711d78a4c942be720d
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-14 16:32:21 +0200

    Added visual separation lines

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -5,11 +5,13 @@
 const findIntersection = (dict1, dict2) => {
   const intersection = {};
   const keysDict1 = Object.keys(dict1);
+
   for (const key of keysDict1) {
     if (dict1[key] === dict2[key]) {
       intersection[key] = dict1[key];
     }
   }
+
   return intersection;
 };
 
