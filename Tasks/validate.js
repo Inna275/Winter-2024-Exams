4848b3968ef68ddfa22e98e24ec70dff0c2972ef
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-15 11:29:48 +0200

    Eliminated big stack of if statements

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -6,11 +6,18 @@ const LOWERCASE_A = 97;
 const LOWERCASE_Z = 122;
 
 const isValid = (name) => {
-  if (!name) return false;
-  if (name === '') return false;
-  if (typeof name !== 'string') return false;
-  if (name.length === 0) return false;
-  if (!name.includes(' ')) return false;
+  const conditions = [
+    !name,
+    name === '',
+    typeof name !== 'string',
+    name.length === 0,
+    !name.includes(' '),
+  ];
+  for (const condition of conditions) {
+    if (condition) {
+      return false;
+    }
+  }
   for (const char of name) {
     if (char === ' ') continue;
     const charCode = char.toLowerCase().charCodeAt(0);
