0299211c0b8d29c14506f23dd9d04d55cfaf3eaa
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-15 10:36:56 +0200

    Removed unnecessary block brackets

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -8,19 +8,17 @@ const isValid = (name) => {
   if (typeof name !== 'string') return false;
   if (name.length === 0) return false;
   if (!name.includes(' ')) return false;
-  {
-    for (const char of name) {
-      if (char === ' ') continue;
-      if (
-        char.toLowerCase().charCodeAt(0) >= 97 &&
-        char.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
+  for (const char of name) {
+    if (char === ' ') continue;
+    if (
+      char.toLowerCase().charCodeAt(0) >= 97 &&
+      char.toLowerCase().charCodeAt(0) <= 122
+    ) {
+    } else {
+      return false;
     }
-    return true;
   }
+  return true;
 };
 
 module.exports = isValid;
